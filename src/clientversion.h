5bc632af9818a730ad96d55d321192b51e276b3c
Easynodecoin <noreply@easynodecoin.io>
2018-11-27 18:43:16 +0100

    version
    version

--- src/clientversion.h
+++ src/clientversion.h
@@ -53,7 +53,7 @@
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
-                         +   10000 * CLIENT_VERSION_MINOR
+                         +   20000 * CLIENT_VERSION_MINOR
                          +     100 * CLIENT_VERSION_REVISION
                          +       1 * CLIENT_VERSION_BUILD;
 
